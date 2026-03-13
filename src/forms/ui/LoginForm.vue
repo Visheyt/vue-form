510843b022df05983f5fff9057840e0ed6d6ff54
Alex <fornikaras7@gmail.com>
2026-03-13 21:52:01 +0500

    feat: add base component styles

---
+++ src/forms/ui/LoginForm.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts"></script>
+
+<template>
+  <form
+    class="form"
+    @submit.prevent=""
+  >
+    <h2>Login Form</h2>
+    <input
+      type="text"
+      placeholder="Введите логин"
+    />
+    <input
+      type="password"
+      placeholder="Введите пароль"
+    />
+    <button type="submit">Войти</button>
+  </form>
+</template>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  max-width: 450px;
+  border: 1px solid rgba(194, 194, 194, 0.82);
+  padding: 20px;
+  border-radius: 8px;
+}
+input {
+  padding: 5px;
+  border: none;
+  border-bottom: 1px solid rgb(187, 186, 186);
+}
+input:focus-visible {
+  outline: none;
+}
+</style>
